ee8945a40b7d890e1c029031cdf8c2ca00eefabe
Danko Papucic <dpapucic@hrsinternational.com>
2020-06-19 11:59:47 +0200

    Age calculator

---
+++ AgeCalculator/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var today = DateTime.Today;
+            
+
+            var age = today.Year - dateTimePicker1.Value.Year;
+            var month = today.Month - dateTimePicker1.Value.Month;
+            var day = (today.Day + dateTimePicker1.Value.Day) - 21;
+
+            if (dateTimePicker1.Value.Date > today.AddYears(-age)) age--;
+            textBox1.Text = Convert.ToString(age);
+            textBox2.Text = Convert.ToString(month);
+            textBox3.Text = Convert.ToString(day);
+        }
+    }
+}
